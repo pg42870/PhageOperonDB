ba1821ba6922548786d13c6895933f41cc272ad2
Joana Gabriel <74028296+pg42870@users.noreply.github.com>
2023-06-26 22:31:28 +0100

    Creation of the PhageOperonDB

---
+++ Construction_of_the_DB/insert_operons.py
@@ -0,0 +1,2 @@
+insert_operons='INSERT INTO PhageOperonDB.Operon (Name, idReferences) VALUES (%s, %s)'
+operon_records= [('gem operon','1'),('Mom operon','2'),('I operon','3'),('lys operon','3'),('P operon','3'),('middle operon','4'),('T7 early operon','5'),('phage lambda leftward operon','5'),('phage lambda rigthward operon','6'),('phage lambda later operon','6'),('lambda rexAB','7'),('gtrABC operon','8'),('PL operon','9'),]
